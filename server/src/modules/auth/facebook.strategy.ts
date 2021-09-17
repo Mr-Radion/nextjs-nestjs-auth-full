4063f72ea3b9970d16eb0ea9e0e0a66424f9e797
radrigis <mr.rodion_oa@mail.ri>
2021-09-17 13:19:09 +0300

    facebook-auth: strategy initial

--- server/src/modules/auth/facebook.strategy.ts
+++ server/src/modules/auth/facebook.strategy.ts
@@ -1,1 +1,37 @@
 // если почты не существует, зарегистрировать напрямую
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor() {
+    super({
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      scope: 'email',
+      profileFields: ['emails', 'name'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
+  }
+}
