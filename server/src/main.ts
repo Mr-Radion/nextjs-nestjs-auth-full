a91d19082ac4d197f33f6c5ad7d3325477cf1598
radrigis <mr.rodion_oa@mail.ri>
2021-09-18 23:17:11 +0300

    swagger: connection for api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,9 +16,9 @@ async function bootstrap() {
     });
     app.setGlobalPrefix('api');
     app.use(cookieParser());
-    // const config = new DocumentBuilder().setTitle('API').setVersion('1.0.0').build(); // openapi info
-    // const document = SwaggerModule.createDocument(app, config);
-    // SwaggerModule.setup('/api/docs', app, document);
+    const config = new DocumentBuilder().setTitle('API').setVersion('1.0.0').setExternalDoc('For more information', 'http://swagger.io').build(); // openapi info
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
     await app.listen(PORT, () => console.log(`Server started on port - ${PORT}`));
   } catch (error) {
     console.log(error);
