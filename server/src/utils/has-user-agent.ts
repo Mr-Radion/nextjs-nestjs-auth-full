17fbd39e40fc079be53ad2b922d9fef0433ec078
radrigis <mr.rodion_oa@mail.ri>
2021-09-27 14:40:21 +0300

    has user agent: adding utild

---
+++ server/src/utils/has-user-agent.ts
@@ -0,0 +1,30 @@
+import { Request } from 'express';
+
+export type UserAgentType = {
+  mobile?: boolean;
+  windows?: boolean;
+  linux?: boolean;
+  other?: boolean;
+};
+
+export const hasUserAgent = (req: Request) => {
+  const ua = req.headers['user-agent'];
+  let result: UserAgentType = {};
+
+  switch (true) {
+    case /mobile/i.test(ua):
+      result['mobile'] = true;
+      break;
+    case /Windows NT/.test(ua):
+      result['windows'] = true;
+      break;
+    case /Linux/.test(ua):
+      result['linux'] = true;
+      break;
+    default:
+      result['other'] = true;
+      break;
+  }
+
+  return result;
+};
